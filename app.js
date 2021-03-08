6d6c2c6058ac5cf70c86143bdbfe6d558176b458
Kasie Omeh <omeh.kasie@gmail.com>
2021-03-09 10:01:08 +1100

    feat 1. Error controllers added

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express()
 const mealRouter = require('./routes/mealRoutes')
 
+const AppError = require('./utils/appError')
+const globalErrorHandler = require ('./controllers/errorController')
 
-
-
+//routes
 app.use(express.json());
-app.use('/api/v1/meals',mealRouter)
+app.use('/api/v1/meals',mealRouter);
+
+//this is here because the valid routes have to work first before this works
+app.all ('*', (req,  res, next) => {
+    next(new AppError(`Cant find ${req.originalUrl}on the server!`,404));
+});
+
+
+app.use(globalErrorHandler)
+
 module.exports = app;
--- models/mealModel.js
+++ models/mealModel.js
@@ -1,22 +1,33 @@
 const mongoose = require("mongoose");
 
 const mealSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        required:[true,'A meal must have a name'],
-        unique: true,
-    },
+  name: {
+    type: String,
+    required: [true, "A meal must have a name"],
+    unique: true,
+  },
+
+  price: {
+    type: Number,
+    required: [true, "A meal must have a price"],
+  },
+  ratings: {
+    type: Number,
+  },
+  imageCover: {
+    type: String,
+    //required: [true, "A meal must have a cover Image "],
+  },
+
+  comments: {
+    type: String,
     
-    price:{ 
-        type: Number,
-        required:[true,'A meal must have a price']
-    },
-    ratings:{
-       type:Number
-
-    }
-
-})
+  },
+  image_url: {
+    type: String,
+   // required: [true, "meal image must be included"],
+  }
+});
 const Meal = mongoose.model('Meal', mealSchema)
 
 module.exports = Meal;
