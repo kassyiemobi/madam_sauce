d7fde045fa9b36d3cd00a26df79395c7d8e6a569
Kasie Omeh <omeh.kasie@gmail.com>
2021-03-10 07:59:09 +1100

    Feat 1.create User model

--- models/userModel.js
+++ models/userModel.js
@@ -1,2 +1,34 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    firstName :{
+        type: String,
+        required: [true,'Please enter First Name']
+    },
+    lastName :{
+        type:String,
+        required:[true,'please enter Last Name']
+    },
+    email :{
+        type: String,
+        required:[true, 'please enter your email'],
+        lowercase:true,
+        validate:[validator.isEmail,'please provide a valid email']
+    },
+    phoneNumber:{
+        type:Number,
+        required:[true, 'please enter a valid number'] 
+       },
+    password:{
+        type:String,
+        required:[true, 'please provide a password'],
+        minlenght:10
+    },
+    photo: String
+})
+
+const user = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+
