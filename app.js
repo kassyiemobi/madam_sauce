c379d303c149ab64eafd0a80afbee84b48b1b16d
Kasie Omeh <omeh.kasie@gmail.com>
2021-03-19 23:46:57 +1100

    security additions

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const AppError = require("./utils/appError");
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
-
-
-const mealRouter = require('./routes/mealRoutes')
-const userRouter = require('./routes/userRoutes')
-const AppError = require('./utils/appError')
 const globalErrorHandler = require ('./controllers/errorController')
 
 
 //Global middlewares
+
+//secure http headers
+app.use(helmet());
+
 //limit requests from same IP
 const limiter =rateLimit({
     max:100,
@@ -20,11 +22,14 @@ const limiter =rateLimit({
 })
 app.use('/api', limiter)
 
-//security HTTP headers
-app.use(helmet())
-
 //body parser.....req.body
 app.use(express.json());
+
+//Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against cross site scripting XSS
+app.use(xss());
 
 //test middle ware
 app.use((req, res, next) => {
@@ -33,8 +38,9 @@ app.use((req, res, next) => {
         next();
 });
 
-
 //routes
+const mealRouter = require("./routes/mealRoutes");
+const userRouter = require("./routes/userRoutes");
 app.use('/api/v1/meals',mealRouter);
 app.use('/api/v1/users',userRouter);
 
